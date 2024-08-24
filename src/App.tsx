42f6485f15bf24151f9115da4cbcb2abdc052b9f
Nam Khuc <khucphuongnam2005@gmail.com>
2024-08-24 16:37:47 +0700

    navbar animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Test from "./Test";
+import Home from "./sections/Home";
 
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fading : {
+          '0%, 100%': {opacity: 1},
+          '50%' : {opacity:0.5},
+        }
+      }
+    },
   },
   plugins: [],
 }
