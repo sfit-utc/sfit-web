a791bf924248f0f60f6261356ca18b649574b5d9
Nam Khuc <khucphuongnam2005@gmail.com>
2024-08-28 14:36:52 +0700

    Done animation for home

---
+++ src/components/Slider.tsx
@@ -0,0 +1,143 @@
+import React, { Component, useEffect } from "react";
+import '../index.css';
+import { useState } from "react";
+
+    // No optimazation
+
+function Slider() {
+    const [slides, setSlides] = useState(Array.from(document.querySelectorAll('.slide')));
+    const [dotList, setDotList] = useState(Array.from(document.querySelectorAll(".dot")))
+
+
+    useEffect(() => {
+        // Convert NodeList to Array
+        const slidesArray = Array.from(document.querySelectorAll('.slide'));
+        setSlides(slidesArray);
+        const dotArray = Array.from(document.querySelectorAll(".dot"));
+        setDotList(dotArray);
+    }, []);
+    let activeIdx = 0;
+
+
+    let prevIdx;
+    const prevButton = () => {
+        prevIdx = activeIdx;
+        activeIdx--;
+        if (activeIdx<0) {
+            activeIdx = slides.length-1;
+            console.log(activeIdx,)
+        }
+        changeSlide(activeIdx)
+    }
+
+    const nextButton = () => {
+        prevIdx = activeIdx;
+        activeIdx++;
+    
+        if (activeIdx>slides.length-1) {
+            activeIdx = 0;
+        }
+        changeSlide(activeIdx)
+
+    }
+
+    const changeSlide = (activeIdx:number) => {
+        slides.forEach((slide,index) => {
+            slide.classList.remove("visible","opacity-100")
+            slide.classList.add("invisible","opacity-0")
+            if (activeIdx == index) {
+                slide.classList.add("visible","opacity-100")
+                slide.classList.remove("invisible","opacity-0")
+            }
+        })
+
+        dotList.forEach((dot,index) => {
+            dot.classList.remove("visible")
+            dot.classList.add("invisible")
+            if (activeIdx == index) {
+                dot.classList.add("visible")
+                dot.classList.remove("invisible")
+            }
+        })
+    }
+
+
+    return (
+        <div className="relative flex items-center justify-center 
+                        w-[35.25rem] h-[19.4375rem]
+                        [&>div]:absolute
+                        [&>div]:w-[30rem] [&>div]:h-[20rem]
+                        [&>div]:shadow-[0px_13px_15.8px_0px_rgba(0,0,0,0.25)] 
+                        [&>div]:bg-cover [&>div]:bg-no-repeat [&>div]:bg-center 
+                        [&>div]:transition-opacity [&>div]:duration-500 [&>div]:ease-in-out">
+
+            {/* Img */}
+            <div className="slide bg-[url(./assets/techclub.jpg)] visible">
+            </div>
+            <div className="slide bg-[url(./assets/hackathonVPBank.jpg)] invisible">
+            </div>  
+            <div className="slide bg-[url(./assets/techclub.jpg)] invisible">
+            </div>
+            <div className="slide bg-[url(./assets/hackathonVPBank.jpg)] invisible">
+            </div>
+ 
+            {/* Button */}
+            <button onClick={prevButton} className="prev absolute left-0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <path d="M28 14C28 11.2311 27.1789 8.52431 25.6406 6.22202C24.1022 3.91973 21.9157 2.12531 19.3576 1.06569C16.7994 0.00605965 13.9845 -0.271187 11.2687 0.269007C8.553 0.8092 6.05844 2.14257 4.10051 4.10051C2.14257 6.05844 0.8092 8.553 0.269007 11.2687C-0.271187 13.9845 0.00605965 16.7994 1.06569 19.3576C2.12531 21.9157 3.91973 24.1022 6.22202 25.6406C8.52431 27.1789 11.2311 28 14 28C17.713 28 21.274 26.525 23.8995 23.8995C26.525 21.274 28 17.713 28 14ZM22 15L9.85 15L15.43 20.607L14 22L6 14L14 6L15.43 7.427L9.85 13L22 13V15Z" fill="#F8F8F8"/>
+                </svg>
+            </button>
+            <button onClick={nextButton} className="next absolute right-0">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                    <path d="M0 14C0 16.7689 0.821086 19.4757 2.35943 21.778C3.89777 24.0803 6.08427 25.8747 8.64243 26.9343C11.2006 27.9939 14.0155 28.2712 16.7313 27.731C19.447 27.1908 21.9416 25.8574 23.8995 23.8995C25.8574 21.9416 27.1908 19.447 27.731 16.7313C28.2712 14.0155 27.9939 11.2006 26.9343 8.64243C25.8747 6.08427 24.0803 3.89777 21.778 2.35943C19.4757 0.821086 16.7689 0 14 0C10.287 0 6.72601 1.475 4.1005 4.1005C1.475 6.72601 0 10.287 0 14ZM6 13H18.15L12.57 7.393L14 6L22 14L14 22L12.57 20.573L18.15 15H6V13Z" fill="#F8F8F8"/>
+                </svg>
+            </button>
+
+            {/* Counter */}
+            <div className="absolute 
+                            [&>div]:absolute
+                            [&>div]:bottom-[-10%]
+                            [&>div]:left-[50%]
+                            [&>div]:translate-x-[-50%]
+                            [&>div]:translate-y-[-50%]">
+                <div className="dot visible">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
+                    <circle cx="4" cy="4" r="4" fill="#808080"/>
+                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
+                  </svg>
+                </div>
+              
+                <div className="dot invisible">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
+                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="18" cy="4" r="4" fill="#808080"/>
+                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
+                  </svg>
+                </div>
+
+                <div className="dot invisible">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
+                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="32" cy="4" r="4" fill="#808080"/>
+                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
+                  </svg>
+                </div>
+
+                <div className="dot invisible">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
+                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
+                    <circle cx="46" cy="4" r="4" fill="#808080"/>
+                  </svg>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Slider;
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,5 +1,6 @@
 import '../index.css';
 import NavBar from '../components/NavBar';
+import Slider from '../components/Slider';
 
 function Home() {
   return (
@@ -53,76 +54,7 @@ function Home() {
             </div>
 
             {/* Slider */}
-            <div className="relative 
-                            flex items-center justify-center 
-                            w-[35.25rem] h-[19.4375rem]
-                            [&>div]:absolute
-                            [&>div]:w-[29rem] [&>div]:h-[16.3125rem]
-                            [&>div]:shadow-[0px_13px_15.8px_0px_rgba(0,0,0,0.25)] 
-                            [&>div]:bg-cover [&>div]:bg-no-repeat [&>div]:bg-center">
-              <div className="bg-[url(./assets/techclub.jpg)] active">
-              </div>
-              <div className="bg-[url(./assets/hackathonVPBank.jpg)]">
-              </div>
-              <div className="bg-[url(./assets/hackathonVPBank.jpg)]">
-              </div>
-              <div className=" bg-[url(./assets/techclub.jpg)]">
-              </div>
-              
-              <button className="absolute left-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
-                  <path d="M28 14C28 11.2311 27.1789 8.52431 25.6406 6.22202C24.1022 3.91973 21.9157 2.12531 19.3576 1.06569C16.7994 0.00605965 13.9845 -0.271187 11.2687 0.269007C8.553 0.8092 6.05844 2.14257 4.10051 4.10051C2.14257 6.05844 0.8092 8.553 0.269007 11.2687C-0.271187 13.9845 0.00605965 16.7994 1.06569 19.3576C2.12531 21.9157 3.91973 24.1022 6.22202 25.6406C8.52431 27.1789 11.2311 28 14 28C17.713 28 21.274 26.525 23.8995 23.8995C26.525 21.274 28 17.713 28 14ZM22 15L9.85 15L15.43 20.607L14 22L6 14L14 6L15.43 7.427L9.85 13L22 13V15Z" fill="#F8F8F8"/>
-                </svg>
-              </button>
-              <button className="absolute right-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
-                  <path d="M0 14C0 16.7689 0.821086 19.4757 2.35943 21.778C3.89777 24.0803 6.08427 25.8747 8.64243 26.9343C11.2006 27.9939 14.0155 28.2712 16.7313 27.731C19.447 27.1908 21.9416 25.8574 23.8995 23.8995C25.8574 21.9416 27.1908 19.447 27.731 16.7313C28.2712 14.0155 27.9939 11.2006 26.9343 8.64243C25.8747 6.08427 24.0803 3.89777 21.778 2.35943C19.4757 0.821086 16.7689 0 14 0C10.287 0 6.72601 1.475 4.1005 4.1005C1.475 6.72601 0 10.287 0 14ZM6 13H18.15L12.57 7.393L14 6L22 14L14 22L12.57 20.573L18.15 15H6V13Z" fill="#F8F8F8"/>
-                </svg>
-              </button>
-
-              <div className="absolute 
-                              [&>div]:absolute
-                              [&>div]:bottom-[-10%]
-                              [&>div]:left-[50%]
-                              [&>div]:translate-x-[-50%]
-                              [&>div]:translate-y-[-50%]">
-                <div className="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
-                    <circle cx="4" cy="4" r="4" fill="#515151"/>
-                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
-                  </svg>
-                </div>
-              
-                <div className="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
-                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="18" cy="4" r="4" fill="#515151"/>
-                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
-                  </svg>
-                </div>
-
-                <div className="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
-                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="32" cy="4" r="4" fill="#515151"/>
-                    <circle cx="46" cy="4" r="4" fill="#D9D9D9"/>
-                  </svg>
-                </div>
-
-                <div className="">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="50" height="8" viewBox="0 0 50 8" fill="none">
-                    <circle cx="4" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="18" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="32" cy="4" r="4" fill="#D9D9D9"/>
-                    <circle cx="46" cy="4" r="4" fill="#515151"/>
-                  </svg>
-                </div>
-              </div>
-            </div>
+            <Slider />
           </div>
         </div>
       </div>
