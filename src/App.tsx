0abd1846019084fec4141f6559e79effee58f8d3
Nam Khuc <khucphuongnam2005@hotmail.com>
2024-08-31 14:12:59 +0700

    Finished Aim

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./sections/Home";
+import Aim from "./sections/Aim";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LandingPage/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -60,8 +60,6 @@ function Home() {
       </div>
     </div>
 
-
-     
   );
 }
 
