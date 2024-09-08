144d8cc7bdcb5f4ccdb9b7c18516b2a805ab8269
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-09-08 08:41:22 +0700

    ADD: Teams

--- src/components/Teams/TeamCard/TeamCard.tsx
+++ src/components/Teams/TeamCard/TeamCard.tsx
@@ -4,6 +4,7 @@ interface TeamCardProps {
         activate: string;
         leader: string;
         image: string;
+        icon: string;
     };
     type: boolean;
 }
@@ -26,60 +27,94 @@ function TeamCard({ team, type }: TeamCardProps) {
 
     return (
         type ? (
-            <div className="transition-transform duration-500 ease-in-out transform hover:scale-105 flex justify-center mb-[100px] translate-x-[-6rem] relative">
-                <div className="w-[1136px] h-[400px] flex justify-center items-center">
-                    <div className="w-[1136px] h-[400px] relative">
-                        <div className="w-[400px] h-[400px] absolute bg-[#d9d9d9] rounded-[30px]" style={{
-                            backgroundImage: `url(${team.image})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                        }} >
-                        </div>
-                        <div className="w-[609px] h-[85px] absolute left-[526px] top-[47px] text-center text-black text-[44px] font-semibold font-['Crimson Text']">
-                            {team.name}
-                        </div>
-                        <div className="w-[610px] h-[166px] absolute left-[526px] top-[203px] text-center">
-                            <span className="text-black text-2xl font-bold font-['Crimson Text']">Hoạt động</span>
-                            <span className="text-black text-2xl font-semibold font-['Crimson Text']"> : {team.activate}</span>
-                        </div>
-                        <div className="w-[610px] h-[45px] absolute left-[526px] top-[132px] text-center">
-                            <span className="text-black text-2xl font-bold font-['Crimson Text']">Trưởng ban </span>
-                            <span className="text-black text-2xl font-semibold font-['Crimson Text']">: {team.leader}</span>
-                        </div>
-                    </div>
-                </div>
-                <div
-                    className="w-[683px] h-[683px] left-[80%] absolute rounded-full blur-[85px]"
-                    style={{
-                        backgroundColor: `${randomColor}40`,
-                    }}
-                />
-            </div>
-        ) : (
-            <div className='transition-transform duration-500 ease-in-out transform hover:scale-105 flex justify-center mb-[100px] translate-x-[6rem] relative'>
-                <div className="w-[1135px] h-[400px] flex justify-center items-center">
-                    <div className="w-[1135px] h-[400px] relative">
-                        <div className="w-[400px] h-[400px] absolute left-[735px] top-0 bg-[#d9d9d9] rounded-[30px]" style={{
+            <div className='duration-500 ease-in-out transform hover:scale-105 mb-[100px]'>
+                <div className="h-[400px]">
+                    <div className="h-[400px] flex mr-[80px] md:ml-[80px] float-start">
+                        <div className="w-[400px] h-[400px] bg-[#d9d9d9] rounded-[30px]" style={{
                             backgroundImage: `url(${team.image})`,
                             backgroundSize: 'cover',
                             backgroundPosition: 'center',
                         }}>
+                            <div className="absolute w-[400px] h-[400px]">
+                                <div style={{
+                                    backgroundImage: `url(${team.icon})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    width: '100px',
+                                    height: '100px',
+                                    position: 'absolute',
+                                    top: '4%',
+                                    left: '87%',
+                                    margin: 'auto',
+                                }}></div>
+                            </div>
                         </div>
-                        <div className="w-[609px] h-[85px] absolute left-0 top-[39px] text-center text-black text-[44px] font-semibold font-['Crimson Text']">
-                            {team.name}
-                        </div>
-                        <div className="w-[610px] h-[166px] absolute left-0 top-[195px] text-center">
-                            <span className="text-black text-2xl font-bold font-['Crimson Text']">Hoạt động</span>
-                            <span className="text-black text-2xl font-semibold font-['Crimson Text']"> : {team.activate}</span>
-                        </div>
-                        <div className="w-[610px] h-[45px] absolute left-0 top-[124px] text-center">
-                            <span className="text-black text-2xl font-bold font-['Crimson Text']">Trưởng ban </span>
-                            <span className="text-black text-2xl font-semibold font-['Crimson Text']">: {team.leader}</span>
+                        <div className="flex justify-center items-center ml-[100px]">
+                            <div>
+                                <div className="w-[609px] h-[85px] text-center text-black text-[44px] font-semibold font-['Crimson Text']">
+                                    {team.name}
+                                </div>
+                                <div className="w-[610px] h-[45px] text-center">
+                                    <span className="text-black text-2xl font-bold font-['Crimson Text']">Trưởng ban </span>
+                                    <span className="text-black text-2xl font-semibold font-['Crimson Text']">: {team.leader}</span>
+                                </div>
+                                <div className="w-[610px] h-[166px] text-center">
+                                    <span className="text-black text-2xl font-bold font-['Crimson Text']">Hoạt động</span>
+                                    <span className="text-black text-2xl font-semibold font-['Crimson Text']"> : {team.activate}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div
-                    className="w-[683px] h-[683px] left-[-10%] absolute rounded-full blur-[85px]"
+                    className="w-[683px] h-[683px] top-[0%] right-[0%] absolute rounded-full blur-[85px]"
+                    style={{
+                        backgroundColor: `${randomColor}40`, // Màu cố định trong trường hợp này
+                    }}
+                />
+            </div>
+        ) : (
+            <div className='duration-500 ease-in-out transform hover:scale-105 mb-[100px]'>
+                <div className="h-[400px]">
+                    <div className="h-[400px] flex md:mr-[80px] float-end">
+                        <div className="flex justify-center items-center mr-[100px]">
+                            <div>
+                                <div className="w-[609px] h-[85px] text-center text-black text-[44px] font-semibold font-['Crimson Text']">
+                                    {team.name}
+                                </div>
+                                <div className="w-[610px] h-[45px] text-center">
+                                    <span className="text-black text-2xl font-bold font-['Crimson Text']">Trưởng ban </span>
+                                    <span className="text-black text-2xl font-semibold font-['Crimson Text']">: {team.leader}</span>
+                                </div>
+                                <div className="w-[610px] h-[166px] text-center">
+                                    <span className="text-black text-2xl font-bold font-['Crimson Text']">Hoạt động</span>
+                                    <span className="text-black text-2xl font-semibold font-['Crimson Text']"> : {team.activate}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-[400px] h-[400px] bg-[#d9d9d9] rounded-[30px]" style={{
+                            backgroundImage: `url(${team.image})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                        }}>
+                            <div className="absolute w-[400px] h-[400px]">
+                                <div style={{
+                                    backgroundImage: `url(${team.icon})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    width: '100px',
+                                    height: '100px',
+                                    position: 'absolute',
+                                    top: '4%',
+                                    left: '-13%',
+                                    margin: 'auto',
+                                }}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="w-[683px] h-[683px] top-[0%] left-[0%] absolute rounded-full blur-[85px]"
                     style={{
                         backgroundColor: `${randomColor}40`, // Màu cố định trong trường hợp này
                     }}
